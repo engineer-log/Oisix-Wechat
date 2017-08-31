22a9fb31237fe8fa148572efcf632a0c2ce5f158
Takeshi <t.fukuda@engineer-log.net>
2017-08-31 13:24:06 +0900

    add file

--- src/templateMessage.java
+++ src/templateMessage.java
@@ -6,20 +6,25 @@ import java.net.URL;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 
-public class tag {
+public class templateMessage {
   public static void main(String[] args) {
     System.out.println("===== HTTP POST Start =====");
     try {
-      URL url = new URL("https://api.weixin.qq.com/cgi-bin/tags/members/batchtagging?access_token=7i46Fke5dDSL1AdjXg_pWu5kn32lKMvLVC-p-9_epakxpD_0UIcgT_beU3Gl3886o6d8WwJ_qmVLdPO3Val_Mpt-JFVeTS3DO1aK3zAbCT5UygewZMQ5TKPQ-dGE3MJdOURjAHAJPM");
+      URL url = new URL("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=UuBJLq_nfSUip_3A4yQv2mHf6hbCKlm2ctXM3-k9idqYGpl1sTg_lhoLU3ttjoYpfcspti2CFc5QTJe2t3jwGCUQTBLFrekKdcEKWyPMfijAMFsW2U-ftzxFmhzU_zoyVCDbAGAAHI");
       HttpURLConnection connection = null;
       try {
         connection = (HttpURLConnection) url.openConnection();
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-
-        //JSON形式の文字列に変換する。
-        String json = "{\"openid_list\":[\"owRRb0rFTbuWSZO-Dkpc4V-NtnSk\"],\"tagid\":100}";
+        String json = "{\"data\":" 
+            + "{\"first\": {\"color\": \"#173177\",\"value\": \"発送通知メッセージです\"}," 
+            + "\"keyword1\": {\"color\": \"#173177\", \"value\": \"福田\"}," 
+            + "\"keyword2\": {\"color\": \"#173177\", \"value\": \"2017年\"}," 
+            + "\"remark\": {\"color\": \"#173177\",\"value\": \"ありがとうございました！\"}}," 
+            + "\"template_id\": \"9kTt0nlO9G11X89nMMRlv9UgrwwNrgbdqUtAuFRiPXw\"," 
+            + "\"touser\": \"owRRb0rFTbuWSZO-Dkpc4V-NtnSk\"," 
+            + "\"url\": \"http://weixin.qq.com\"}";
         PrintStream ps = new PrintStream(connection.getOutputStream());
         ps.print(json);
         ps.close();
